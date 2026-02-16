8f1d4aa8a50c2cace0934a531b34577377a1101b
Justin Fivor <371340896@qq.com>
2026-02-16 10:47:03 +0800

    Remove getSession from PublicLayout to make root page static

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,13 +1,10 @@
 import { Header } from '@/components/layout/Header';
-import { getSession } from '@/lib/session';
 
 export default async function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getSession();
-
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       {/* <Header user={session} />  Removed Header as per requirement */}
